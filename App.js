80b462f9abc4a2d41caa68171677a864cee19f87
MrBLAZIKEN <57632375+MrBLAZIKEN@users.noreply.github.com>
2021-03-22 19:03:36 -0600

    Robando las cosas de Dani carita feliz

--- App.js
+++ App.js
@@ -2,10 +2,13 @@ import 'react-native-gesture-handler';
 import React from 'react';
 //////////////////////
 import PerfilPremium from './src/Movil/Vistas/PerfilPremium';
+import PerfilTrabajador from './src/Movil/Vistas/PerfilTrabajador';
 //////////////////////
 
 export default App = () => {
   return (
-      <PerfilPremium />
+       /*<PerfilPremium />
+       Aquí se ponen las cosas para acomodarlas después no? */
+      <PerfilTrabajador />
   );
 };
