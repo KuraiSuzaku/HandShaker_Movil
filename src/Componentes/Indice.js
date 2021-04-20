dda0b84fcb323dd44d2c578e8883ccc3c9f4ca18
Daniel Castañeda Mejia <gaming.sub04@gmail.com>
2021-04-20 18:39:43 -0500

    fixed register import in components index

--- src/Componentes/Indice.js
+++ src/Componentes/Indice.js
@@ -2,7 +2,7 @@ import EncabezadoApp from './EncabezadoApp';
 import Navegacion from './Navegacion';
 import Login from './Login/Login';
 import LogOut from './Login/LogOut';
-import Registro from './Registro/EscogerTipoUsuario';
+import Registro from './Registro/RegistroCliente';
 import * as PerfilCliente from './PerfilCliente/Indice';
 import * as PerfilPremium from './PerfilPremium/Indice';
 import * as PerfilTrabajador from './PerfilTrabajador/Indice';
