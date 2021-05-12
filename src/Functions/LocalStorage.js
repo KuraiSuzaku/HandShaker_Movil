7c3af33194e3e2dd93e708cb37daab1573fa6b8a
Daniel Castañeda Mejia <gaming.sub04@gmail.com>
2021-05-12 12:24:23 -0500

    added storeUser and getUser for local storage

--- src/Functions/LocalStorage.js
+++ src/Functions/LocalStorage.js
@@ -1,2 +1,20 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
+const storeUser = async (value) => {
+    try {
+        await AsyncStorage.setItem('@storage_Key', value)
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+const getUser = async () => {
+    try {
+        const value = await AsyncStorage.getItem('@storage_Key')
+        if(value !== null) {
+        return value;
+        }
+    } catch(e) {
+        console.error(e);
+    }
+}
