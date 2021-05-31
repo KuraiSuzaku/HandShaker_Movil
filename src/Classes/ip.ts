7afbdbc7a503f388e394267e9ac95e554166616c
Daniel Castañeda Mejia <gaming.sub04@gmail.com>
2021-05-31 10:10:40 -0500

    git cache reset

--- src/Classes/ip.ts
+++ src/Classes/ip.ts
@@ -1,3 +1,3 @@
 
-export const rooturl = 'http://192.168.1.72:3001/api/';
-export const SERVER = 'http://192.168.1.72:3001';
+export const rooturl = 'http://192.168.0.201:3001/api/';
+export const SERVER = 'http://192.168.0.201:3001';
